3fb1d181f300a1009bd437ca0a54f79d97734d65
Vignesh <reactivicky@gmail.com>
2024-06-10 21:13:50 +0530

    added feature to get todos

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 import { config } from 'dotenv';
 config();
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import fs from 'fs';
 import { param, validationResult } from 'express-validator';
 import { rateLimit } from 'express-rate-limit';
 import createConnection from './createConnection.js';
-
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
+import { getAllToursQuery, getTourQuery } from './queries/getTours.js';
 
 const app = express();
+const client = createConnection();
 
 // Middleware to parse JSON bodies, limited to 10KB
 app.use(express.json({ limit: '10kb' }));
@@ -23,29 +20,30 @@ const limiter = rateLimit({
   max: 100, // limit each IP to 100 requests per windowMs
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get('/api/v1/tours', limiter, (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
+app.get('/api/v1/tours', limiter, async (req, res) => {
+  try {
+    const toursRes = await client.query(getAllToursQuery);
+    const tours = toursRes.rows;
+    res.status(200).json({
+      status: 'success',
+      results: toursRes.rowCount,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      message: error,
+    });
+  }
 });
 
 app.get(
   '/api/v1/tours/:id',
   limiter,
-  param('id')
-    .isAlphanumeric()
-    .withMessage('id must be alphanumeric')
-    .trim()
-    .escape(),
-  (req, res) => {
+  param('id').isNumeric().withMessage('id must be numeric').trim().escape(),
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ status: 'failed', errors: errors.array() });
@@ -53,18 +51,25 @@ app.get(
     // If validation passed, proceed with the request handling
     const tourId = req.params.id;
     // Fetch and return tour data by ID
-    const tour = tours[tourId];
-    if (tour) {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour,
-        },
-      });
-    } else {
+    try {
+      const tourRes = await client.query(getTourQuery, [tourId]);
+      if (tourRes.rowCount !== 0) {
+        res.status(200).json({
+          status: 'success',
+          data: {
+            tour: tourRes.rows,
+          },
+        });
+      } else {
+        res.status(404).json({
+          status: 'failed',
+          message: `Tour with id ${tourId} does not exist`,
+        });
+      }
+    } catch (error) {
       res.status(404).json({
         status: 'failed',
-        message: `Tour with id ${tourId} does not exist`,
+        message: error,
       });
     }
   }
@@ -72,32 +77,12 @@ app.get(
 
 app.post('/api/v1/tours', limiter, (req, res) => {
   console.log(req.body);
-  const newId = tours.at(-1).id + 1;
-  const newTour = { ...req.body, id: newId };
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        res.status(404).json({
-          status: 'failed',
-          message: 'Something went wrong!',
-        });
-        return;
-      }
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
   try {
-    await createConnection().connect();
+    await client.connect();
     console.log('Connected to db...');
     console.log(`App listening on port ${port}...`);
   } catch (error) {
---
+++ queries/getTours.js
@@ -0,0 +1,53 @@
+export const getAllToursQuery = `
+  SELECT 
+      t.id,
+      t.name,
+      t.duration,
+      t.maxGroupSize,
+      t.difficulty,
+      t.ratingsAverage,
+      t.ratingsQuantity,
+      t.price,
+      t.summary,
+      t.description,
+      t.imageCover,
+      ARRAY_AGG(DISTINCT td.start_date) AS start_dates,
+      ARRAY_AGG(DISTINCT ti.image) AS images
+  FROM 
+      tours t
+  LEFT JOIN 
+      tour_dates td ON t.id = td.tour_id
+  LEFT JOIN 
+      tour_images ti ON t.id = ti.tour_id
+  GROUP BY 
+      t.id
+  ORDER BY 
+      t.id;
+`;
+
+export const getTourQuery = `
+  SELECT 
+      t.id,
+      t.name,
+      t.duration,
+      t.maxGroupSize,
+      t.difficulty,
+      t.ratingsAverage,
+      t.ratingsQuantity,
+      t.price,
+      t.summary,
+      t.description,
+      t.imageCover,
+      ARRAY_AGG(DISTINCT td.start_date) AS start_dates,
+      ARRAY_AGG(DISTINCT ti.image) AS images
+  FROM 
+      tours t
+  LEFT JOIN 
+      tour_dates td ON t.id = td.tour_id
+  LEFT JOIN 
+      tour_images ti ON t.id = ti.tour_id
+  WHERE
+      t.id = $1
+  GROUP BY 
+      t.id;
+`;
