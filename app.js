2865c1f4c41fdda7586f3cb086c4980913e8a2c2
Vignesh <reactivicky@gmail.com>
2024-06-14 11:18:18 +0530

    added eslint and validations

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import { config } from 'dotenv';
 config();
 import express from 'express';
-import { param, validationResult, body } from 'express-validator';
+import { param, validationResult } from 'express-validator';
 import { rateLimit } from 'express-rate-limit';
 import createConnection from './createConnection.js';
-import { getAllToursQuery, getTourQuery, insertTour } from './queries/tours.js';
+import { getAllToursQuery, getTourQuery } from './queries/tours.js';
+import { createTourValidation } from './validations/createTour.js';
 
 const app = express();
 const client = createConnection();
@@ -18,6 +19,8 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
+
+const tourValidationsArray = createTourValidation();
 
 app.get('/api/v1/tours', limiter, async (req, res) => {
   try {
@@ -74,99 +77,42 @@ app.get(
   }
 );
 
-app.post(
-  '/api/v1/tours',
-  limiter,
-  [
-    // Validate request body fields using express-validator
-    body('name')
-      .isString({ max: 255 })
-      .withMessage('name should be string')
-      .notEmpty()
-      .withMessage('Name is required')
-      .trim()
-      .escape(),
-    body('duration')
-      .notEmpty()
-      .withMessage('duration is required')
-      .isInt({ min: 1 })
-      .withMessage('duration must be a positive integer'),
-    body('maxGroupSize')
-      .notEmpty()
-      .withMessage('maxGroupSize is required')
-      .isInt({ min: 1 })
-      .withMessage('maxGroupSize must be a positive integer'),
-    body('difficulty')
-      .notEmpty()
-      .withMessage('difficulty is required')
-      .isIn(['easy', 'medium', 'difficult'])
-      .withMessage('difficulty must be easy, medium, or difficult'),
-    body('price')
-      .notEmpty()
-      .withMessage('price is required')
-      .isFloat({ min: 0 })
-      .withMessage('price must be a positive number'),
-    body('summary')
-      .notEmpty()
-      .withMessage('summary is required')
-      .isString({ max: 255 })
-      .withMessage('summary should be string')
-      .trim()
-      .escape(),
-    body('description')
-      .notEmpty()
-      .withMessage('description is required')
-      .isString({ max: 1000 })
-      .withMessage('description should be string')
-      .trim()
-      .escape(),
-    body('imageCover')
-      .isString({ max: 255 })
-      .withMessage('imageCover should be string')
-      .trim()
-      .escape(),
-    body('startDates').isArray().withMessage('Start dates must be an array'),
-    body('startDates.*').isISO8601().withMessage('Invalid start date format'),
-    body('tourImages').isArray().withMessage('Tour images must be an array'),
-    body('tourImages.*').isURL().withMessage('Invalid image URL'),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ status: 'failed', errors: errors.array() });
-    }
-
-    const {
-      name,
-      duration,
-      maxGroupSize,
-      difficulty,
-      price,
-      summary,
-      description,
-      imageCover,
-      startDates,
-      tourImages,
-    } = req.body;
-
-    console.log({
-      name,
-      duration,
-      maxGroupSize,
-      difficulty,
-      price,
-      summary,
-      description,
-      imageCover,
-      startDates,
-      tourImages,
-    });
-
-    // try {
-    //   const tourRes = await client.query(insertTour, []);
-    // } catch (error) {}
+app.post('/api/v1/tours', limiter, tourValidationsArray, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ status: 'failed', errors: errors.array() });
   }
-);
+
+  const {
+    name,
+    duration,
+    maxGroupSize,
+    difficulty,
+    price,
+    summary,
+    description,
+    imageCover,
+    startDates,
+    tourImages,
+  } = req.body;
+
+  console.log({
+    name,
+    duration,
+    maxGroupSize,
+    difficulty,
+    price,
+    summary,
+    description,
+    imageCover,
+    startDates,
+    tourImages,
+  });
+
+  // try {
+  //   const tourRes = await client.query(insertTour, []);
+  // } catch (error) {}
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, async () => {
