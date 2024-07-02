37b64f7e09c81afc77a8861872b00b099402a9df
Vignesh <reactivicky@gmail.com>
2024-07-02 08:18:16 +0530

    added static file serving

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { rateLimit } from 'express-rate-limit';
 import tourRouter from './routes/tourRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Middleware to parse JSON bodies, limited to 10KB
 app.use(express.json({ limit: '10kb' }));
 // Middleware to parse URL-encoded bodies, limited to 10KB
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(express.static(`${__dirname}/public`));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
