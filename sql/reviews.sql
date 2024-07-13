e5996198e8dad5f7e3935fbbd782df4f54f2ed4e
Vignesh <reactivicky@gmail.com>
2024-07-13 13:11:09 +0530

    package.json changes

--- sql/reviews.sql
+++ sql/reviews.sql
@@ -4,6 +4,6 @@ CREATE TABLE reviews (
   rating INT CHECK (rating >= 1 AND rating <= 5),
   user_id INT NOT NULL,
   tour_id INT NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(_id) ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   FOREIGN KEY (tour_id) REFERENCES tours(id) ON DELETE CASCADE
 );
